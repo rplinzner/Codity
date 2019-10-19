c8fd3c764bf6ce888e3791ac9c3ddb501beea06b
Damian Salata <damiansalata10@gmail.com>
2019-10-19 19:22:47 +0200

    Add Base Response

---
+++ Twitter.BE/src/Twitter.Shared/ResponseModels/BaseResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Twitter.Shared.ResponseModels.Interfaces;
+
+namespace Twitter.Shared.ResponseModels
+{
+    public class BaseResponse : IBaseResponse
+    {
+        public string Message { get; set; }
+        public bool IsError { get => Errors.Count > 0; }
+        public ICollection<Error> Errors { get; set; } = new List<Error>();
+
+        public void AddError(Error error)
+        {
+            Errors.Add(error);
+        }
+    }
+}
