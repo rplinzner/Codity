860ecf5a216ba8972a31e9f44e4b2ff46fce5889
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-11-14 14:08:01 +0100

    [Add] translations for register auth

--- Twitter.FE/programmers-twitter/src/components/containers/Authentication/register.tsx
+++ Twitter.FE/programmers-twitter/src/components/containers/Authentication/register.tsx
@@ -5,10 +5,18 @@ import { toast } from 'react-toastify';
 import { TextField } from 'formik-material-ui';
 import { Button, Container } from '@material-ui/core';
 import ReCAPTCHA from 'react-google-recaptcha';
+import {
+  withLocalize,
+  Translate as T,
+  LocalizeContextProps,
 
-import * as constants from "../../../constants/global.constats";
+} from 'react-localize-redux';
 
-interface Props {}
+import * as constants from '../../../constants/global.constats';
+
+import { authTranslations } from '../../../translations/index';
+
+interface Props extends LocalizeContextProps {}
 interface State {
   canSubmit: boolean;
 }
@@ -20,7 +28,12 @@ interface FormValues {
   password: string;
 }
 
-export default class extends Component<Props, State> {
+class Register extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.props.addTranslation(authTranslations);
+  }
+
   state: State = { canSubmit: false };
 
   onSubmit = (values: FormValues): void => {
@@ -31,94 +44,109 @@ export default class extends Component<Props, State> {
     this.setState({ canSubmit: true });
   };
 
+  getSchema = (translate: any) => {
+     return Yup.object({
+       firstName: Yup.string().required(translate('firstNameRequired')),
+       lastName: Yup.string().required(translate('lastNameRequired')),
+       email: Yup.string()
+         .email(translate('emailValid'))
+         .required(translate('emailRequired')),
+       password: Yup.string().required(translate('passwordRequired')),
+       passwordConfirm: Yup.string()
+         .oneOf([Yup.ref('password'), null], translate('passwordsMustMatch'))
+         .required(translate('passwordConfirmRequired')),
+     });
+  }
+
   render() {
-    const schema = Yup.object({
-      firstName: Yup.string().required('To be filled'),
-      lastName: Yup.string().required('Firld '),
-      email: Yup.string()
-        .email('valid must be')
-        .required(),
-      password: Yup.string().required(),
-      passwordConfirm: Yup.string()
-        .oneOf([Yup.ref('password'), null], 'Passwords must match translation')
-        .required('confirm your password'),
-    });
+   
 
     return (
       <Container>
-        <Formik
-          initialValues={{
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-            passwordConfirm: '',
-          }}
-          validationSchema={schema}
-          onSubmit={this.onSubmit}
-          render={({ submitForm, isSubmitting }) => (
-            <Form>
-              <Field
-                name="firstName"
-                type="text"
-                label="First Name translation"
-                component={TextField}
-                margin="normal"
-                fullWidth
-              />
-              <br />
-              <Field
-                name="lastName"
-                type="text"
-                label="Last Name translation"
-                component={TextField}
-                margin="normal"
-                fullWidth
-              />
-              <br />
-              <Field
-                name="email"
-                type="email"
-                label="Email"
-                component={TextField}
-                margin="normal"
-                fullWidth
-              />
-              <br />
-              <Field
-                name="password"
-                type="password"
-                label="password translation"
-                autoComplete="new-password"
-                component={TextField}
-                margin="normal"
-                fullWidth
-              />
-              <br />
-              <Field
-                name="passwordConfirm"
-                type="password"
-                autoComplete="new-password"
-                label="passwordConfirm translation"
-                component={TextField}
-                margin="normal"
-                fullWidth
-              />
-              <br />
-              <ReCAPTCHA
-                onChange={this.onCaptchaSubmitted}
-                sitekey={constants.RecaptchaSiteKey}
-              />
-              <Button disabled={isSubmitting || !this.state.canSubmit} variant="contained" color="primary" onClick={submitForm}>
-                Submit
-              </Button>
-              <Button variant="contained" color="secondary" type="reset">
-                Submit
-              </Button>
-            </Form>
+        <T>
+          {({ translate }) => (
+            <Formik
+              initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                passwordConfirm: '',
+              }}
+              validationSchema={this.getSchema(translate)}
+              onSubmit={this.onSubmit}
+              render={({ submitForm, isSubmitting }) => (
+                <Form>
+                  <Field
+                    name="firstName"
+                    type="text"
+                    label={<T id="firstName" />}
+                    component={TextField}
+                    margin="normal"
+                    fullWidth
+                  />
+                  <br />
+                  <Field
+                    name="lastName"
+                    type="text"
+                    label={<T id="lastName" />}
+                    component={TextField}
+                    margin="normal"
+                    fullWidth
+                  />
+                  <br />
+                  <Field
+                    name="email"
+                    type="email"
+                    label="Email"
+                    component={TextField}
+                    margin="normal"
+                    fullWidth
+                  />
+                  <br />
+                  <Field
+                    name="password"
+                    type="password"
+                    label={<T id="password" />}
+                    autoComplete="new-password"
+                    component={TextField}
+                    margin="normal"
+                    fullWidth
+                  />
+                  <br />
+                  <Field
+                    name="passwordConfirm"
+                    type="password"
+                    autoComplete="new-password"
+                    label={<T id="passwordConfirm" />}
+                    component={TextField}
+                    margin="normal"
+                    fullWidth
+                  />
+                  <br />
+                  <ReCAPTCHA
+                    onChange={this.onCaptchaSubmitted}
+                    sitekey={constants.RecaptchaSiteKey}
+                  />
+                  <Button
+                    disabled={isSubmitting || !this.state.canSubmit}
+                    variant="contained"
+                    color="primary"
+                    onClick={submitForm}
+                  >
+                    {<T id="sent" />}
+                  </Button>
+                  <Button variant="contained" color="secondary" type="reset">
+                    {<T id="reset" />}
+                  </Button>
+                </Form>
+              )}
+            />
           )}
-        />
+        </T>
       </Container>
     );
   }
 }
+
+export default withLocalize(Register);
