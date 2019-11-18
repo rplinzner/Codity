f057baa1610d575352577c8bfc94602e82c17543
rplinzner <rplinzner@gmail.com>
2019-11-18 16:56:16 +0100

    [Fix] Paper transparency

--- Twitter.FE/programmers-twitter/src/components/containers/additional/404-page.tsx
+++ Twitter.FE/programmers-twitter/src/components/containers/additional/404-page.tsx
@@ -38,7 +38,7 @@ const useStyles = makeStyles(() =>
       top: '15%',
     },
     paper: {
-      backgroundColor: 'rgba(255, 255, 255, 0.4)',
+      backgroundColor: 'rgba(255, 255, 255, 0.5)',
       padding: '20px',
       marginRight: '10px',
     },
