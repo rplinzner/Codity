cfd89bfaa6dd5a43894e36723f5c11ea32775cf4
rplinzner <rplinzner@gmail.com>
2019-11-20 23:18:32 +0100

    [Add] Translations for layout components

--- Twitter.FE/programmers-twitter/src/translations/index.ts
+++ Twitter.FE/programmers-twitter/src/translations/index.ts
@@ -1,2 +1,3 @@
 export { default as globalTranslations } from './global.json';
 export { default as authTranslations } from './auth.json';
+export { default as layoutTranslations } from './layout.json';
